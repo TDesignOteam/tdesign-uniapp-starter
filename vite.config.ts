5e7216776962c2613e90f7caf6ecee9d3b0c2a88
novlan1 <1576271227@qq.com>
2026-02-08 15:35:07 +0800

    chore: use postcss-config-remove-selector

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 import uni from '@dcloudio/vite-plugin-uni';
+import {
+  postCssPluginRemoveSelector,
+  TDESIGN_ICON_REMOVE_SELECTOR,
+  TDESIGN_USED_ICONS,
+} from '@novlan/postcss-plugin-remove-selector';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +17,28 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        // tdesign-uniapp 图标减包插件
+        postCssPluginRemoveSelector({
+          list: [
+            {
+              ...TDESIGN_ICON_REMOVE_SELECTOR.list[0],
+              include: [
+                ...TDESIGN_USED_ICONS,
+                'chat-double',
+                'chart-bar',
+                'user-add',
+              ],
+            },
+          ],
+          // 开启调试模式可查看处理日志
+          debug: true,
+        }),
+      ],
     },
   },
 });
