c2a80d890e2eaee1e8869dbbf7a6c447ef8dece1
novlan1 <1576271227@qq.com>
2026-03-23 17:08:42 +0800

    chore: update deps

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createSSRApp } from 'vue';
 
 import App from './App.vue';
 import '@tdesign/uniapp/common/style/theme/index.less';
+// 等待新版本发布
+// import '@tdesign/uniapp/theme.less';
 import './styles/index.less';
 
 const chooseImage = uni.chooseImage || {};
